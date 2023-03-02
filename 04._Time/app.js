01ed11597bce87e353e26e54fbf38af84392ddb6
SkibstedN <niko933n@stud.kea.dk>
2023-03-02 21:37:34 +0100

    Final edit time project

--- 04._Time/app.js
+++ 04._Time/app.js
@@ -10,17 +10,6 @@ app.get("/", (req, res) => {
 
 
 
-
-/* const PORT = 8080;
-app.listen(PORT, (error) =>  {
-    if (error){
-        console.log(error);
-        return;
-    }
-    console.log("Server is running on ", PORT);
-});   */
-
-
 const PORT = 5000;
 app.listen(5000, () => {
     console.log("Running on port 5000.");

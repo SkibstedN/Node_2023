c81c46a5c5ea7104e43574b7b3a7f243930b17cb
SkibstedN <niko933n@stud.kea.dk>
2023-03-10 09:01:02 +0100

    clientside redirection

--- 03._Plain_HTML_/public/frontpage/frontpage.js
+++ 03._Plain_HTML_/public/frontpage/frontpage.js
@@ -1,1 +1,12 @@
-console.log("Thank you for visiting");
+//console.log("Thank you for visiting");
+
+
+//assigment redirect a user to visitors after they have been in the museum for 3 seconds
+
+// set the redirect URL
+//const redirectUrl = "visitors/visitors.html";
+
+// use setTimeout to redirect after 4 seconds
+setTimeout(() => {
+  window.location.href = "visitors/visitors.html";
+}, 3000);
